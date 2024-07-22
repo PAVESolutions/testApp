cca1e4e7024d466047fb91a9aefb7118d205d246
PAVESolutions <kenric.tan@pavesolutions.sg>
2024-07-22 16:23:31 +0800

    change config

--- testApp.cpp
+++ testApp.cpp
@@ -12,8 +12,8 @@ int main() {
     cv::namedWindow("Display", cv::WINDOW_GUI_NORMAL | cv::WINDOW_NORMAL);
     cv::setWindowProperty("Display", cv::WND_PROP_FULLSCREEN, cv::WINDOW_GUI_EXPANDED);
     cv::VideoCapture camera(0);
-    camera.set(cv::CAP_PROP_FRAME_WIDTH, 3264);
-    camera.set(cv::CAP_PROP_FRAME_HEIGHT, 2448);
+    camera.set(cv::CAP_PROP_FRAME_WIDTH, 1600);
+    camera.set(cv::CAP_PROP_FRAME_HEIGHT, 1200);
     ImageUnwrapper unwrapper("config.txt");
     cv::Mat frame;
     while (true) {
